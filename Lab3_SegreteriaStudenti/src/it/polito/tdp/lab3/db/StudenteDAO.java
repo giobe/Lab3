e591bfe2849c4ef5b7a0ec5b1e4968e7a8329268
giovanni gui <giovanni@192.168.0.109>
2016-04-05 13:12:36 +0200

    versione in classe

--- Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/db/StudenteDAO.java
+++ Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/db/StudenteDAO.java
@@ -1,5 +1,51 @@
 package it.polito.tdp.lab3.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import it.polito.tdp.lab3.model.Studente;
+
 public class StudenteDAO {
 
+	public void create(Studente s){
+		Connection conn = DBConnect.getConnection();
+		String sql = "INSERT INTO `iscritticorsi`.`studente` (`matricola`, `cognome`, `nome`, `CDS`) "
+				+ "VALUES (?, ?, ?, ?);";
+		PreparedStatement st;
+		try {
+			st = conn.prepareStatement(sql);
+			st.setInt(1, Integer.parseInt(s.getMatricola()));
+			st.setString(2, s.getCognome());
+			st.setString(3, s.getNome());
+			st.setString(4, s.getCDS());
+			
+			st.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * legge da db lo studente con @param matricola = a s.matricla e poi riepie tutti i campi ad essa legati
+	 * se non lo trova 
+	 * 
+	 * @return
+	 */
+	public Studente read(int matricola){
+		return null;
+	}
+	/**
+	 * questo cerca solo, verifica presenza studente, true se non c'è invece false
+	 * @param s
+	 * @return
+	 */
+	public boolean read(Studente s){
+		return false;
+	}
+	public void updateCds(Studente s){}
+	public List<Studente> searchByNome(String nome){
+		return null;
+	}
 }
--- Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/model/Studente.java
+++ Lab3_SegreteriaStudenti/src/it/polito/tdp/lab3/model/Studente.java
@@ -29,9 +29,39 @@ public class Studente {
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
+	
+	public String getCDS() {
+		return CDS;
+	}
+	public void setCDS(String cDS) {
+		CDS = cDS;
+	}
 	@Override
 	public String toString() {
 		return  matricola + " " + nome + " " + cognome + " " + CDS + "\n";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((matricola == null) ? 0 : matricola.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Studente other = (Studente) obj;
+		if (matricola == null) {
+			if (other.matricola != null)
+				return false;
+		} else if (!matricola.equals(other.matricola))
+			return false;
+		return true;
 	}
 	
 
